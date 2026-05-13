0cfe1eb8ddf63eda0c96ea24d041a20dcdd67d97
PeakVision0814 <perished_hgp@qq.com>
2026-05-13 19:52:50 +0800

    Update Prompt

--- 4_Agent_Classical_Paradigm_Construction/ReAct.py
+++ 4_Agent_Classical_Paradigm_Construction/ReAct.py
@@ -95,5 +95,5 @@ if __name__ == '__main__':
     search_desc = "一个网页搜索引擎。当你需要回答关于时事、事实以及在你的知识库中找不到的信息时，应使用此工具。"
     tool_executor.registerTool("Search", search_desc, search)
     agent = ReActAgent(llm_client=llm, tool_executor=tool_executor)
-    question = "华为最新的手机是哪一款？它的主要卖点是什么？"
+    question = "当前日期：2026年5月13日。请注意，用户可能需要最新的信息。华为最新的手机是哪一款？它的主要卖点是什么？"
     agent.run(question)
